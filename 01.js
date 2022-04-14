c0b046dd6e0507ccc1508249401cd63148414cfc
lurkgabriel <gabriel60020111@gmail.com>
2022-04-14 05:34:50 -0300

    exercicio 1 com logica em vez de recurso da lingaguem

--- 01.js
+++ 01.js
@@ -1,11 +1,19 @@
 function findBiggestValue(){
     let array = []
+    let aux = 0
 
     for(let i = 0; i < 20; i++){
         array[i] = Math.random()*100
+
+        if(i==0){
+            aux = array[i]
+        }
+		if(array[i] > aux){
+            aux = array[i]
+        }
     }
     
-    console.log("Maior valor: ", Math.max(...array))
+    console.log("Maior valor: ", aux)
 }
 
 findBiggestValue()
